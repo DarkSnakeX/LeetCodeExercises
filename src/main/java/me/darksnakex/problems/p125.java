881327db2a560cbfe4119a1c7920dd94d4661098
DarkSnakeX <52959327+DarkSnakeX@users.noreply.github.com>
2025-07-25 01:59:59 +0200

    Add solutions for problems 3, 13, 125, and 680

---
+++ src/main/java/me/darksnakex/problems/p125.java
@@ -0,0 +1,22 @@
+package me.darksnakex.problems;
+
+public class p125 {
+
+    public boolean isPalindrome(String s) {
+
+        String res = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        int der = res.length() - 1;
+        for (int izq = 0; izq < res.length() / 2; izq++) {
+            // Caso de letras no iguales
+            if (res.charAt(der) != res.charAt(izq)) {
+                return false;
+            }
+            der--;
+
+        }
+
+        return true;
+    }
+
+}
